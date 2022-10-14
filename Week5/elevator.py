4c43f4857640c582a72860c78d881d60c25c6512
Yodahe Mulugeta <mul21017@byui.edu>
2022-10-14 11:50:06 -0600

    oc

---
+++ Week5/elevator.py
@@ -0,0 +1,11 @@
+#my goal is to creat menu for the ethiopian food I sell
+#I want the website to make it easier for my coustomers pick their order nad contact me to place their order
+#Have you ever tries ethiopian food? If you have, we are sure you loved it, but if not that is okay becasue you found the rigth place to try it your first time.
+#we make fresh ethiopian food with different rich tastes. We made this website so it can be easier for our customers to access put menue and make it easeir to order. 
+'''
+#Do you know anything about Ethiopia? It is in east Africa or the horn of Africa and offers a variety of 
+traditional foods that are flavorful. Have you ever tried food from Ethiopia? If you've tried it, we're certain 
+you enjoyed it, but if not, that's fine since you came to the right location to try it for the first time.
+We cook authentic Ethiopian food with a variety of flavorful flavors. We created this website to make it easier
+ for our customers to access the menu and place orders.
+'''
